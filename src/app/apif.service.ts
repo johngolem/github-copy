814ad1c6736c4c5f3ee6f317c231db2163787370
John Paul <johnpaul@trees.local>
2021-07-28 12:07:35 +0300

    finally working

--- src/app/apif.service.ts
+++ src/app/apif.service.ts
@@ -1,9 +1,7 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, } from 'rxjs';
-import { retry } from 'rxjs/operators';
-import { catchError } from 'rxjs/operators'
-import { throwError } from 'rxjs';
+
 
 
 @Injectable({
@@ -15,39 +13,13 @@ export class ApifService {
 // how it will fetch the github profile from search
 
 public gitProfile(searchQuery: any):Observable<any>{
-  let dataURL = 'https://api.github.com/users/${searchQuery}';
-  return this.httpClient.get <any>(dataURL).pipe(
-   retry (1),
-   catchError(this.handleErrors)
-  );
-}
-
+  let dataURL = `https://api.github.com/users/${searchQuery}`
+  return this.httpClient.get <any>(dataURL)
+   }
   // how it will fetch the github repos from search
 
 public gitRepos(searchQuery: any):Observable<any[]> {
-    let dataURL = 'https://api.github.com/users/${searchQuery}/repos';
-    return this.httpClient.get<any[]>(dataURL).pipe(
-    retry(1),
-    );
- }
-public handleErrors(error:HttpErrorResponse){
-    let errorMessage:string;
-    if (error.error instanceof ErrorEvent){
-      //error from client side
-      errorMessage = 'MESSAGE : ${error.error.message}';
-      }
-    else{
-    // error from the servers 
-    errorMessage= 'STATUS : ${error.status} MESSAGE : ${error.message }';
-  }
-   return throwError(errorMessage);
-
-    }
-  
-
-  
+    let dataURL = `https://api.github.com/users/${searchQuery}/repos`
+    return this.httpClient.get<any[]>(dataURL)
+   }
 }
-    
-/*function retry(count: any, arg1: number): import("rxjs").OperatorFunction<ArrayBuffer, unknown> {
-  throw new Error('Function not implemented.');
-} */
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { ApifService } from '../apif.service';
-import { catchError } from 'rxjs/operators'
+
 
 
 @Component({
@@ -14,23 +13,19 @@ export class FormComponent implements OnInit {
 public githubUserQuery:string;
 public githubProfile:any;
 public githubRepos:any[];
-public errorMessage:string;
 
   constructor(private githubService:ApifService) {}
   
 public searchUserInfo(){
 // to get github profile  
+
   this.githubService.gitProfile(this.githubUserQuery).subscribe((data)=>{this.githubProfile = data;
-  },(error)=>{
-    this.errorMessage = error;
+    
   });
         //to get github repo
   this.githubService.gitRepos(this.githubUserQuery).subscribe((data)=>{this.githubRepos = data;
-  },(error)=>{
-    this.errorMessage = error;
   });
 }
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   }
